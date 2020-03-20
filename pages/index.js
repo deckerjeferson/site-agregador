d527e8797fbe27654f1edca15c36800347950a02
Vinicius Costa <viniciusrdacosta@gmail.com>
2020-03-20 12:37:19 -0300

    Adiciona estrutura inicial do projeto

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Index = () => (
+  <div className="container">
+    <Head>
+      <title>Site Agregador de Editais COVID-19</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>
+        Site Agregador de Editais
+      </h1>
+    </main>
+  </div>
+);
+export default Index
